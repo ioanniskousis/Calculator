da1db184689be6b59cc3b2ec817b618937392478
Ioannis Kousis <bonobo@otenet.gr>
2020-09-09 14:39:10 +0300

    defaultProps for App class

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
@@ -10,15 +11,26 @@ class App extends React.Component {
   }
 
   render() {
+    const { result } = this.props;
     return (
       <div>
         <div className="App">
-          <Display />
+          <Display
+            result={result}
+          />
           <ButtonPanel />
         </div>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  result: PropTypes.string,
+};
+
+App.defaultProps = {
+  result: '0',
+};
 
 export default App;
