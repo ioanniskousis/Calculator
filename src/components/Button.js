3d01d1b7174f7c8326b27e0de39463531c73d917
Ioannis Kousis <bonobo@otenet.gr>
2020-09-09 14:46:40 +0300

    markup & default props for Button class

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   constructor(props) {
@@ -7,12 +8,32 @@ class Button extends React.Component {
   }
 
   render() {
+    const { name } = this.props;
+    const { sign } = this.props;
+    const { dblwidth } = this.props;
+    const { orange } = this.props;
+    const classname = (dblwidth ? 'Button Button2' : 'Button')
+      .concat(orange ? ' Button3' : '');
     return (
-      <div>
-        {}
+      <div className={classname} id={name}>
+        {sign}
       </div>
     );
   }
 }
+
+Button.propTypes = {
+  name: PropTypes.string,
+  sign: PropTypes.string,
+  dblwidth: PropTypes.bool,
+  orange: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  name: '',
+  sign: '',
+  dblwidth: false,
+  orange: false,
+};
 
 export default Button;
